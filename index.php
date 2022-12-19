768eed47be421a50a1848dccdf4f3a80185e0c91
PaoloWD <paolo.lore.93@gmail.com>
2022-12-19 17:25:20 +0100

    add styles

--- index.php
+++ index.php
@@ -7,13 +7,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>BADWORDS</title>
  </head>
- <body>
- <form action="script.php" method="POST">
-    <textarea name="message" ></textarea>
-    <input name="" type="submit" value="Invia">
-</form>
+ <body class="bg-dark">
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <div class="mb-3">
+                <form action="script.php" class="text-center" method="POST">
+                    <label for="exampleFormControlTextarea1" class="form-label"></label>
+                    <textarea class="form-control" name="message" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <button class="btn btn-primary mt-3">INVIA</button>
+                </form>
+            </div>
+        </div>
+    </div>
+ 
 
     
  </body>
--- script.php
+++ script.php
@@ -1,4 +1,22 @@
 <?php $messageForm = $_POST["message"];
 $messageReplaced = str_replace("cane", "***", $messageForm);
  ?>
-Il testo è: <?php echo $messageReplaced ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="bg-dark">
+    <div class="row justify-content-center">
+        <div class="col-4 text-white text-center">
+            Il testo con le censure è: <?php echo $messageReplaced ?>
+        </div>
+    </div>
+</body>
+</html>
